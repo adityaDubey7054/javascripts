7c005bc5fd5fbcc1a48ab5b74664ef00f81aeab2
Aditya Dubey <94119092+adityaDubey7054@users.noreply.github.com>
2025-05-16 07:30:16 +0000

    new added

---
+++ basic/conversionType.js
@@ -0,0 +1,26 @@
+let mark = "100";
+ console.log(typeof mark);
+ console.log(typeof (mark));
+let score = Number(mark)
+ console.log(typeof score)
+ console.log(score)
+/*
+   if mark = 33 - number
+      mark = "33abc"-NaN
+      mark = null - 0
+      mark = undefined - NaN
+
+*/
+let islogout = "";
+let Anishislogout = Boolean(islogout);
+console.log(Anishislogout);
+console.log(typeof Anishislogout)
+/*
+  value =1   - true
+  value =0    - false
+  value = null,undefined,""    - false
+  */
+ let age = 23;
+ let ageconvert = String(age)
+ console.log(ageconvert)
+ console.log(typeof ageconvert)
