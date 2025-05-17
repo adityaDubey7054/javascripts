36aa352e48b1d72553acb9fa91d3e9614a01c024
Aditya Dubey <94119092+adityaDubey7054@users.noreply.github.com>
2025-05-17 19:32:35 +0000

    new file added

---
+++ basic/dates.js
@@ -0,0 +1,23 @@
+let date = new Date();
+console.log(date.toDateString());
+console.log(date.toISOString());
+console.log(date.toLocaleDateString());
+console.log(date.toJSON());
+console.log(date.toLocaleString());
+console.log(date.toLocaleTimeString());
+console.log(date.toString());
+console.log(date.toTimeString());
+console.log(date.toUTCString());
+let newdate = new Date();
+console.log(newdate.getDate());
+console.log(newdate.getTimezoneOffset());
+
+
+
+
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+// let myCreatedDate = new Date("01-14-2023")
+console.log(myCreatedDate.toLocaleString());
